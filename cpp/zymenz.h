68a377ba2c8b9683cf1e365404e02305a0969bdf
Yoel Sitorus | ዘሜቲያ <sclous2012@gmail.com>
2021-09-10 09:34:40 +0700

    Add files via upload

---
+++ cpp/zymenz.h
@@ -0,0 +1,10 @@
+#ifndef ZYMENZ_H
+#define ZYMENZ_H
+#include <iostream>
+	class zymenz{
+		public:
+			std::string alph="q?+elP53h4pfL_b:kQ/rZBmg9sj;HUvnXR=JNDEW8KoTG2iSMc67YI#xzFdaACVw-1!0tOyu.";
+			std::string zencode(std::string kata);
+			std::string zdecode(std::string code);
+	};
+#endif
