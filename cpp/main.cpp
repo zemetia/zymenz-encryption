4f74289c2f3a35481bf07a37a167230d4344714e
Yoel Sitorus | ዘሜቲያ <sclous2012@gmail.com>
2021-09-10 09:37:30 +0700

    Add files via upload

---
+++ cpp/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "zymenzx.h"
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	zymenz code;
+	cout<<code.zencode("hlao");
+	return 0;
+}
